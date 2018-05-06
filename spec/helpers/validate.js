c24d827ae62f20e9102ed38d375392bd5554f95b
Alex Robinson <github@alex.fu2k.org>
2018-05-06 01:08:44 +0100

    Add tests

---
+++ spec/helpers/validate.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const Linter = require('eslint').Linter
+const linter = new Linter()
+const settings = require(path.join(__dirname, '..', '..', 'index'))
+
+const validate = code => {
+  const result = linter.verify(code, settings)
+  // console.log('WAHEY!!!!!', result)
+  result.valid = result.length === 0
+  return result
+}
+
+module.exports = validate
